3fd952830e063f4d5a25695d327cf28f4177c23c
baijinghua <501704831@qq.com>
2018-08-23 19:45:12 +0800

    添加实体类shop

---
+++ src/main/java/com/qf/shop/pojo/po/Shop.java
@@ -0,0 +1,23 @@
+package com.qf.shop.pojo.po;
+
+public class Shop {
+
+    private int age;
+    private  String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
